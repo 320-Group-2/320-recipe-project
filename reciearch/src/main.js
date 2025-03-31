a5c2cd4eb95a41f1467fa0c5cb4b2f6fd3c8b8df
Eric Gu <ejgu@umass.edu>
2025-03-31 15:56:33 -0400

    switched to React (accidently used HTML) in navbar :(

---
+++ reciearch/src/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+
+// Create a router with the routes
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  }
+]);
+
+// Render the application
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
